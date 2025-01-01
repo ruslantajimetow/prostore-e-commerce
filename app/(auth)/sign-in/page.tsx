edeb4b54f6e21b3ac5c30cf411b203c5391129f2
Ruslan <tajimetowruslan@gmail.com>
2025-01-01 23:34:01 +0800

    modified for pd

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -2,7 +2,6 @@ import logo from '@/public/images/logo.svg';
 import { Metadata } from 'next';
 import Link from 'next/link';
 import Image from 'next/image';
-import { APP_NAME } from '@/lib/constants';
 import {
   Card,
   CardContent,
@@ -20,7 +19,15 @@ export default function SignInPage() {
   return (
     <Card className="w-full max-w-md mx-auto">
       <CardHeader className="flex-center space-y-4">
-        <Image src={logo} alt="logo" width={100} height={100} priority={true} />
+        <Link href="/" target="_self">
+          <Image
+            src={logo}
+            alt="logo"
+            width={100}
+            height={100}
+            priority={true}
+          />
+        </Link>
         <CardTitle className="text-center">Sign In</CardTitle>
       </CardHeader>
       <CardDescription className="text-center">
