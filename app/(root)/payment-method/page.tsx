61a65316896f28c949152c4d1e3a31aeaefcc5d1
Ruslan <tajimetowruslan@gmail.com>
2025-01-12 19:37:53 +0800

    payment method page

--- app/(root)/payment-method/page.tsx
+++ app/(root)/payment-method/page.tsx
@@ -1,5 +1,25 @@
-import React from 'react';
+import { Metadata } from 'next';
+import { getUserById } from '@/lib/actions/user.actions';
+import { auth } from '@/auth';
+import PaymentMethodForm from './payment-method-form';
+import CheckOutSteps from '@/components/shared/check-out-steps';
 
-export default function page() {
-  return <div>page</div>;
+export const metadata: Metadata = {
+  title: 'Payment Method',
+};
+
+export default async function page() {
+  const session = await auth();
+  const userId = session?.user?.id;
+
+  if (!userId) throw new Error('User id not found');
+
+  const user = await getUserById(userId);
+
+  return (
+    <div>
+      <CheckOutSteps current={2} />
+      <PaymentMethodForm prefferredPaymentMethod={user.paymantMethod} />
+    </div>
+  );
 }
