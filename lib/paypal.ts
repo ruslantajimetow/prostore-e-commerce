adb1778fc2bcbf6c5a4b60df0d6f3e8909c57466
Ruslan <tajimetowruslan@gmail.com>
2025-01-15 22:39:51 +0800

    paypal tests and generate token tests

--- lib/paypal.ts
+++ lib/paypal.ts
@@ -1,15 +1,63 @@
 const base = process.env.PAYPAL_API_URL || 'https://api-m.sandbox.paypal.com';
 
-export const paypal = {};
+export const paypal = {
+  createOrder: async function createOrder(price: number) {
+    const accessToken = await generatePaypalToken();
+    const url = `${base}/v2/checkout/orders`;
+
+    const response = await fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${accessToken}`,
+      },
+      body: JSON.stringify({
+        intent: 'CAPTURE',
+        purchase_units: [
+          {
+            amount: {
+              currency_code: 'USD',
+              value: price,
+            },
+          },
+        ],
+      }),
+    });
+
+    if (response.ok) {
+      return await response.json();
+    } else {
+      const errmsg = await response.text();
+      throw new Error(errmsg);
+    }
+  },
+  capturePayment: async function capturePayment(orderId: string) {
+    const accessToken = await generatePaypalToken();
+    const url = `${base}/v2/checkout/orders/${orderId}/capture`;
+
+    const response = await fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${accessToken}`,
+      },
+    });
+
+    if (response.ok) {
+      return await response.json();
+    } else {
+      const errmsg = await response.text();
+      throw new Error(errmsg);
+    }
+  },
+};
 
 //Generate paypal access token
 
 async function generatePaypalToken() {
-  const { PAYPAL_CLIENT_ID, PAYPAL_SECRET } = process.env;
-
-  const auth = Buffer.from(`${PAYPAL_CLIENT_ID}:${PAYPAL_SECRET}`).toString(
-    'base64'
-  );
+  const auth = Buffer.from(
+    `${process.env.PAYPAL_CLIENT_ID}:${process.env.PAYPAL_APP_SECRET}`
+  ).toString('base64');
 
   const response = await fetch(`${base}/v1/oauth2/token`, {
     method: 'POST',
@@ -20,6 +68,8 @@ async function generatePaypalToken() {
     },
   });
 
+  console.log(response);
+
   if (response.ok) {
     const jsonData = await response.json();
     return jsonData.access_token;
@@ -28,3 +78,5 @@ async function generatePaypalToken() {
     throw new Error(errormsg);
   }
 }
+
+export { generatePaypalToken };
