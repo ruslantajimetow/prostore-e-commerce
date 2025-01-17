d0e6a3bc669e4c699337ff13176320ab463e78f7
Ruslan <tajimetowruslan@gmail.com>
2025-01-17 19:54:16 +0800

    user orders and profile pages

---
+++ app/user/main-nav.tsx
@@ -0,0 +1,39 @@
+'use client';
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+import { cn } from '@/lib/utils';
+import React from 'react';
+
+const links = [
+  {
+    title: 'Profile',
+    href: '/user/profile',
+  },
+  {
+    title: 'Orders',
+    href: '/user/orders',
+  },
+];
+
+export default function MainNav({
+  className,
+  ...props
+}: React.HtmlHTMLAttributes<HTMLElement>) {
+  const pathName = usePathname();
+  return (
+    <nav className={cn('flex items-center space-x-4 lg:space-x-6', className)}>
+      {links.map((link, i) => (
+        <Link
+          key={i}
+          href={link.href}
+          className={cn(
+            'text-md font-medium transition-colors hover:text-primary',
+            pathName.includes(link.href) ? '' : 'text-muted-foreground'
+          )}
+        >
+          {link.title}
+        </Link>
+      ))}
+    </nav>
+  );
+}
---
+++ app/user/orders/page.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function UserOrdersPage() {
+  return <div>USerOrdersPage</div>;
+}
