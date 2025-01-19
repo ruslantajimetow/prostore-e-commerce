5feeb7b97cc7c8e33a9bbfeed077bd9678a47c3e
Ruslan <tajimetowruslan@gmail.com>
2025-01-19 14:10:54 +0800

    user update name action is added

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -140,3 +140,36 @@ export const updateUserPaymentMethod = async (
     };
   }
 };
+
+export const updateUserProfile = async (user: {
+  name: string;
+  email: string;
+}) => {
+  try {
+    const session = await auth();
+    if (!session) throw new Error('User Not Authorized');
+
+    const currentUser = await prisma.user.findFirst({
+      where: { id: session.user?.id },
+    });
+
+    if (!currentUser) throw new Error('User Not Found');
+
+    await prisma.user.update({
+      where: { id: currentUser.id },
+      data: {
+        name: user.name,
+      },
+    });
+
+    return {
+      success: true,
+      message: 'User Name updated successfully',
+    };
+  } catch (error) {
+    return {
+      success: false,
+      message: formatError(error),
+    };
+  }
+};
