7d2c2db0282c2b70ba87cfda77f689e2609f6b60
Ruslan <tajimetowruslan@gmail.com>
2025-01-29 22:41:53 +0800

    priceFlter and ratingFilter

--- lib/actions/product.actions.ts
+++ lib/actions/product.actions.ts
@@ -70,10 +70,29 @@ export async function getAllProducts({
           } as Prisma.StringFilter,
         }
       : {};
+  const priceFilter: Prisma.ProductWhereInput =
+    price && price !== 'all'
+      ? {
+          price: {
+            gte: Number(price.split('-')[0]),
+            lte: Number(price.split('-')[1]),
+          },
+        }
+      : {};
+  const ratingFilter: Prisma.ProductWhereInput =
+    rating && rating !== 'all'
+      ? {
+          rating: {
+            gte: Number(rating),
+          },
+        }
+      : {};
   const data = await prisma.product.findMany({
     where: {
       ...queryFilter,
       ...categoryFilter,
+      ...priceFilter,
+      ...ratingFilter,
     },
     orderBy: { createdAt: 'desc' },
     skip: (page - 1) * limit,
@@ -87,6 +106,7 @@ export async function getAllProducts({
   return {
     data: JSON.parse(JSON.stringify(data)) as Product[],
     totalPages: Math.ceil(dataCount / limit),
+    sort,
   };
 }
 
